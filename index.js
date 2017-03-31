330b87114f49b0b04aa03ed92b839b3b339ddbe7
Mie O <miei1@outlook.com>
2017-03-31 04:13:50 +0100

    some refactoring, fix abnormalities matched for multiple party members

--- index.js
+++ index.js
@@ -4,87 +4,95 @@ if(debug){
     delete require.cache[require.resolve('./lib/cooldown')]
     delete require.cache[require.resolve('./lib/entity')]
     delete require.cache[require.resolve('./lib/party')]
+    delete require.cache[require.resolve('./lib/notify')]
 }
 
 const AbnormalManager = require('./lib/abnormal')
 const CooldownManager = require('./lib/cooldown')
 const EntityManager = require('./lib/entity')
 const PartyManager = require('./lib/party')
+const Notify = require('./lib/notify')
 
 module.exports = function BattleNotify(dispatch){
+    let enabled = false
     const abMan = new AbnormalManager(dispatch, debug)
     const cooldown = new CooldownManager(dispatch, debug)
     const entities = new EntityManager(dispatch, debug)
     const party = new PartyManager(dispatch, debug)
-    let events = []
-    let enabled = false
-
-    const targets = {
-        self: function(cb){
-            cb(entities.self().cid)
-        },
-        myboss: function(cb) {
-            cb(entities.myBoss())
-        },
-        partyincludingself: function(cb) {
-            party.members().forEach(cb)
-        },
-        party: function(cb){
-            party.members().forEach(cid => {
-                if(cid === entities.self().cid) return
-                cb(cid)
-            })
-        }
-    }
-
+    const notify = new Notify(dispatch, debug)
+    const events = new Set()
     const conditions = {
-        expiring: function(args){
-            let timesToMatch = args.timeRemaining || 6
-            if(typeof timesToMatch !== typeof []) timesToMatch = [timesToMatch]
-            return function(info){
-                if(!info || !info.expires) return false
-                let remaining = Math.round((info.expires - Date.now())/1000)
-                if(timesToMatch.includes(remaining)) return (info.refreshed || info.added) + (remaining * 1000)
-            }
-        },
-        added: function(args) {
-            return function(info) { return (info ? info.added : false) }
-        },
-        addedorrefreshed: function(args){
-            let requiredStacks = args.requiredStacks || 1
-            return function(info) {
-                if(info && info.stacks < requiredStacks) return false
-                return (info ? (info.refreshed || info.added) : false)
-            }
-        },
-        refreshed: function(args){
-            let requiredStacks = args.requiredStacks || 1
-            return function(info) {
-                if(info && info.stacks < requiredStacks) return false
-                return (info ? info.refreshed : false)
-            }
-        },
-        removed: function(args){
-            return function(info) { return (info ? info.removed : false) }
-        },
-        missing: function(args){
-            let rewarnTimeout = args.rewarnTimeout || 5
-            rewarnTimeout *= 1000
-            return function(info, lastMatch) {
-                if(!info || (info && info.removed)){
-                    if((lastMatch + rewarnTimeout) > Date.now()) return
-                    return (Date.now())
+        abnormal: {
+            expiring: function(args){
+                let timesToMatch = args.timeRemaining || 6
+                if(typeof timesToMatch !== typeof []) timesToMatch = [timesToMatch]
+                return function(info){
+                    if(!info || !info.expires) return false
+                    let remaining = Math.round((info.expires - Date.now())/1000)
+                    if(timesToMatch.includes(remaining)) return (info.refreshed || info.added) + (remaining * 1000)
+                }
+            },
+            added: function(args) {
+                return function(info) { return (info ? info.added : false) }
+            },
+            addedorrefreshed: function(args){
+                const requiredStacks = args.requiredStacks || 1
+                return function(info) {
+                    if(info && info.stacks < requiredStacks) return false
+                    return (info ? (info.refreshed || info.added) : false)
+                }
+            },
+            refreshed: function(args){
+                const requiredStacks = args.requiredStacks || 1
+                return function(info) {
+                    if(info && info.stacks < requiredStacks) return false
+                    return (info ? info.refreshed : false)
+                }
+            },
+            removed: function(args){
+                return function(info) { return (info ? info.removed : false) }
+            },
+            missing: function(args){
+                const rewarnTimeout = (args.rewarnTimeout || 5)*1000
+                return function(info, lastMatch) {
+                    if(!info || (info && info.removed)){
+                        if((lastMatch + rewarnTimeout) > Date.now()) return
+                        return (Date.now())
+                    }
+                }
+            },
+            missingduringcombat: function(args){
+                const rewarnTimeout = (args.rewarnTimeout || 5)*1000
+                return function(info, lastMatch) {
+                    if(!info || (info && info.removed)){
+                        if((lastMatch + rewarnTimeout) > Date.now()) return
+                        if(!entities.myEntity().combat) return
+                        return Date.now()
+                    }
                 }
             }
         },
-        missingduringcombat: function(args){
-            let rewarnTimeout = args.rewarnTimeout || 5
-            rewarnTimeout *= 1000
-            return function(info, lastMatch) {
-                if(!info || (info && info.removed)){
-                    if((lastMatch + rewarnTimeout) > Date.now()) return
-                    if(!entities.myEntity().combat) return
-                    return Date.now()
+        cooldown: {
+            expriring: function(args){
+
+            }
+        },
+        targets: {
+            self: function(cb){
+                cb(entities.self().cid)
+            },
+            myboss: function(cb) {
+                cb(entities.myBoss())
+            },
+            partyincludingself: function(cb) {
+                for(const cid of party.members()){
+                    cb(cid)
+                }
+            },
+            party: function(cb){
+                const myCid = entities.self().cid
+                for(const cid of party.members()) if(cid !== myCid) {
+                    cb(cid)
                 }
             }
         }
@@ -104,88 +112,76 @@ module.exports = function BattleNotify(dispatch){
     })
 
     function AbnormalEvent(abnormals, target, type, message, args){
-        if(typeof abnormals !== typeof []) abnormals = [abnormals]
-        type = type.toLowerCase()
-        target = target.toLowerCase()
-        let iterateTargets = targets[target]
-        let condition = conditions[type](args)
-        let lastMatch = 0
+        const iterateTargets = conditions.targets[target]
+        const condition = conditions.abnormal[type](args)
+        const lastMatches = new Map()
 
         function checkEntity(id){
+            if(!id) return
             const entity = entities.get(id)
             if(entity.dead) return
-            let results = []
+            id = id.toString()
+            if(!lastMatches.has(id)) lastMatches.set(id, 0)
+            const results = new Set()
+
             let info
-            let _lastMatch = lastMatch
-            abnormals.forEach(abnormal => {
-                const match = condition(abMan.get(id, abnormal), lastMatch)
-                if(match && match !== lastMatch){
-                    _lastMatch = match
+            let lastMatch = lastMatches.get(id)
+
+            for(const abnormal of abnormals){
+                const match = condition(abMan.get(id, abnormal), lastMatches.get(id))
+                if(match && match !== lastMatches.get(id)){
+                    lastMatch = match
                     info = abMan.get(id, abnormal)
-                    results.push(true)
-                } else results.push(false)
-            })
-            if(type.includes('missing')){
-                if(results.every(result => { return result })){
-                    // for "missing" types, we need all abnormalities to be missing
-                    notifyAbnormal(message, entity, info)
-                    lastMatch = _lastMatch
-                }
-            } else if (results.includes(true)){
-                //if one abnormality matched
-                notifyAbnormal(message, entity, info)
-                lastMatch = _lastMatch
+                    results.add(true)
+                } else results.add(false)
             }
+
+            if(type.includes('missing') && results.has(false) || !results.has(true)) return
+
+            notify.abnormal(message, entity, info)
+            lastMatches.set(id, lastMatch)
         }
         this.check = function(){
             iterateTargets(checkEntity)
         }
     }
-    function notifySkillReset(message, info){
-        if(info){
-            message = message.replace('{icon}', info.icon)
-        }
-        notify(message)
+
+    function CooldownEvent(groups, type, message, args){
+        // nyi
     }
-    function notifyAbnormal(message, entity, info){
-        if(info){
-            message = message.replace('{duration}', Math.round((info.expires - Date.now())/1000) + 's')
-            message = message.replace('{stacks}', info.stacks)
-        }
-        if(entity && entity !== {}){
-            message = message.replace('{name}', entity.name)
-            message = message.replace('{nextEnrage}',  entity.nextEnrage + '%')
-        }
-        notify(message)
+
+    function createAbnormalEvent(abnormals, target, type, message, arg){
+        if(typeof abnormals !== typeof []) abnormals = [abnormals]
+        type = type.toLowerCase()
+        target = target.toLowerCase()
+
+        events.add(new AbnormalEvent(abnormals, target, type, message, arg))
     }
-    function notify(message){
-        dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
-            unk1: 2,
-            unk2: 0,
-            unk3: 0,
-            message
-        })
-        dispatch.toClient('S_CHAT',1, {
-              channel: 203,
-              authorID: { high: 0, low: 0 },
-              unk1: 0,
-              gm: 0,
-              unk2: 0,
-              authorName: '',
-              message: message,
+    function createCooldownEvent(skills, type, message, args){
+
+    }
+    function createResetEvent(skills, message, args){
+        if(typeof skills !== typeof []) skills = [skills]
+        let groups = new Set()
+
+        for(const skill of skills){
+            groups.add(Math.floor(skill / 10000))
+        }
+        cooldown.onReset(groups, (info) => {
+            notify.skillReset(message, info)
         })
     }
     function refreshConfig(){
-        events = []
+        events.clear()
         cooldown.clearResetHooks()
         delete require.cache[require.resolve('./config/common')]
         delete require.cache[require.resolve('./config/' + entities.self().class)]
         loadEvents(require('./config/common'))
         loadEvents(require('./config/' + entities.self().class))
     }
-    function loadEvents(obj){
-        obj.forEach(event => {
-            if(event.abnormalities)
+    function loadEvents(events){
+        for(const event of events){
+            if(event.abnormalities){
                 createAbnormalEvent(
                     event.abnormalities,
                     event.target,
@@ -197,32 +193,23 @@ module.exports = function BattleNotify(dispatch){
                         requiredStacks: event.required_stacks
                     }
                 )
-            if(event.type && event.type.toLowerCase() === 'reset')
+                continue
+            }
+            if(event.type && event.type.toLowerCase() === 'reset'){
                 createResetEvent(
                     event.skills,
                     event.message,
                     {}
                 )
-        })
-    }
-    function createAbnormalEvent(abnormals, target, type, message, arg){
-        events.push(new AbnormalEvent(abnormals, target, type, message, arg))
-    }
-    function createResetEvent(skills, message, args){
-        if(typeof skills !== typeof []) skills = [skills]
-        let groups = new Set()
-        skills.forEach(skill => {
-            groups.add(Math.floor(skill / 10000))
-        })
-        cooldown.onReset(groups, (info) => {
-            notifySkillReset(message, info)
-        })
+                continue
+            }
+        }
     }
     function checkEvents(){
         if(!enabled) return
-        events.forEach(event => {
+        for(const event of events){
             event.check()
-        })
+        }
     }
     const checkTimer = setInterval(checkEvents, 500)
     this.destructor = function(){
