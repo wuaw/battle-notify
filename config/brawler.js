2f3231830a23e45b2a49212a4d325b1f6a1de676
Mie O <miei1@outlook.com>
2017-04-02 12:45:24 +0100

    add vergos stacks to tank configs

--- config/brawler.js
+++ config/brawler.js
@@ -1,1 +1,20 @@
-module.exports = []
+module.exports = [
+
+    // Vergos Aggro Debuff
+   {
+		type: 'AddedOrRefreshed',
+		target: 'PartyIncludingSelf',
+		abnormalities: 950023,
+		message: '{name} has {stacks} stacks',
+		required_stacks: 1
+	},
+
+	// Vergos Aggro Debuff Expire
+   {
+		type: 'Expiring',
+		target: 'PartyIncludingSelf',
+		abnormalities: 950023,
+		message: '{name}\'s stacks are expiring',
+		time_remaining: 4
+	},
+]
--- config/warrior.js
+++ config/warrior.js
@@ -16,4 +16,24 @@ module.exports = [
 		message: '{icon} Reset'
 	},
 */
+
+/*
+	// Vergos Aggro Debuff
+   {
+		type: 'AddedOrRefreshed',
+		target: 'PartyIncludingSelf',
+		abnormalities: 950023,
+		message: '{name} has {stacks} stacks',
+		required_stacks: 1
+	},
+
+	// Vergos Aggro Debuff Expire
+   {
+		type: 'Expiring',
+		target: 'PartyIncludingSelf',
+		abnormalities: 950023,
+		message: '{name}\'s stacks are expiring',
+		time_remaining: 4
+	},
+*/
 ]
